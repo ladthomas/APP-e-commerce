66bde54adcd5617ebc4306d1f25fcc58b705cfae
ladthomas <ladouyouthomas@outlook.fr>
2024-07-09 20:56:37 +0200

    modified

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,14 @@
 module.exports = {
-  chainWebpack: (config) => {
+  chainWebpack: config => {
     config.module
-      .rule("vue")
-      .use("vue-loader")
-      .tap((options) => {
+      .rule('vue')
+      .use('vue-loader')
+      .loader('vue-loader')
+      .tap(options => {
         options.compilerOptions = {
-          isCustomElement: (tag) => tag.startsWith("ion-"),
+          isCustomElement: tag => tag.startsWith('ion-')
         };
         return options;
       });
-  },
-  devServer: {
-    Proxy: "https://3540-160-155-147-112.ngrok-free.app",
   }
 };
